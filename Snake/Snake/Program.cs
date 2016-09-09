38d0ce0b8d9716afd28c7400edf9aae8b7b8db0f
exotik997 <exotik.qwerty@gmail.com>
2016-09-10 01:53:57 +0300

    Добавляем еду и удлиняем змейку при поедании

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,17 +26,33 @@ namespace Snake
             //Отрисовка змейки
             Point p = new Point (4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();      
-        
+            snake.Draw(); 
+            
+            //Отрисовка еды
+            FoodCreator foodCreator = new FoodCreator(150, 40, '#');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while(true)
             {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+
+                Thread.Sleep(50);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(10);
-                snake.Move();
             }
         }
     }
