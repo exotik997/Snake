3c7da90c9860b189f9176dcdede49b386177fee0
exotik997 <exotik.qwerty@gmail.com>
2016-09-09 22:39:49 +0300

    Изучаем списки, работаем с классами

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,29 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1 , 3, '*');
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Console.ReadLine();
+            List<int> numList = new List<int>();
+            numList.Add(15);
+            numList.Add(23);
+            numList.Add(19);
 
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
+            Console.ReadLine();
         }
     }
 }
